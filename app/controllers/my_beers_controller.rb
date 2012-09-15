4e9c47ecc5e9ac145a3c55e09ae39669a148fb79
Dyan Carra <dyancarra@gmail.com>
2012-09-15 13:53:05 -0300

    add protect to my_beers controller, add destroy method in my_beers

--- app/controllers/my_beers_controller.rb
+++ app/controllers/my_beers_controller.rb
@@ -1,10 +1,18 @@
 class MyBeersController < ApplicationController
 
+  before_filter :logged_in
+
   def add
     my_beer = MyBeer.new(:user_id => params[:user_id], :beer_id => params[:beer_id])
     if my_beer.save
       redirect_to root_path
     end
+  end
+  
+  def destroy
+    get_my_beer = MyBeer.find(params[:id])
+    get_my_beer.destroy
+    redirect_to root_path
   end
 
 end

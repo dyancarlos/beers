48793f9b78ce21cfe0f0eaef9f4fa03c9e670c7c
Dyan Carra <dyancarra@gmail.com>
2012-09-14 21:48:11 -0300

    add method logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :logged_in
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+  
+  def logged_in
+    unless current_user
+      redirect_to "http://google.com"
+    end
+  end
+  helper_method :current_user, :logged_in
 end
